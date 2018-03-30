c77b6e78130d366599695653990f465a56d4c8c1
Christopher Pauli <chpa5089@colorado.edu>
2018-03-30 12:14:21 -0600

    Comment Update on Mapping.sh

--- Mapping.sh
+++ Mapping.sh
@@ -1,8 +1,17 @@
+#Make sure to have trimmed files named {File_Name}_Trimmed.fastq.gz 
+
+#Sed command creates a base file name for all files downstreamed to be named properly
+
 FILES=`ls *Trimmed.fastq.gz | sed 's/.fastq.gz//g'`
 for F in $FILES ; do
 
+#Map the Reads to Transcriptome
 bwa mem ../Trinity.fasta ${F}.fastq.gz > ${F}.sam
+
+#Transform Sam to Bam File
 samtools view ${F}.sam ${F}.bam
+
+#Generate Statistics for the alignment and save those as a txt file
 samtools flagstat ${F}.bam > ${F}.txt
 
 done
